60c1d0dd0860086c9f44fe7662355932349d0bc4
Nils Wloka <nils.wloka@codecentric.de>
2016-09-15 16:47:06 +0200

    Created components from design mockup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
-import './App.css';
+import './normalize.css';
+import './skeleton.css';
+import './custom.css';
+import burger from '../img/burger.png';
+import cart from '../img/cart.png';
+import close from '../img/close.png';
+import down from '../img/down.png';
+import search from '../img/search.png';
 import { Grid, Navbar, Nav, NavItem, Button } from 'react-bootstrap';
 import { Router, Route, IndexRoute, IndexLink, browserHistory } from 'react-router';
 import { LinkContainer } from 'react-router-bootstrap';
 import { connect, Provider } from 'react-redux';
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap/dist/css/bootstrap-theme.css';
 import { createStore } from 'redux';
 import AuthService from './AuthService'
 
@@ -17,10 +22,26 @@ function increment() {
   }
 }
 
-function counterReducer(state = 0, action) {
+function showOverlay() {
+  return {
+    type: 'SHOW_OVERLAY'
+  }
+}
+
+function hideOverlay() {
+  return {
+    type: 'HIDE_OVERLAY'
+  }
+}
+
+function counterReducer(state = {counter: 0, showOverlay: false}, action) {
   switch (action.type) {
     case 'INCREMENT':
-      return state + 1;
+      return Object.assign({}, state, {counter: state.counter + 1});
+    case 'SHOW_OVERLAY':
+      return Object.assign({}, state, {showOverlay: true});
+    case 'HIDE_OVERLAY':
+      return Object.assign({}, state, {showOverlay: false});
     default:
       return state;
   }
@@ -28,37 +49,88 @@ function counterReducer(state = 0, action) {
 
 let store = createStore(counterReducer);
 
+class Teaser extends Component {
+  render () {
+    const callToAction = !this.props.small ? <button className="button">{this.props.callToAction}</button> : <a className="action-link">{this.props.callToAction}</a>;
+    const subHeadline = this.props.small ? [] : <p className="sub-headline">{this.props.subHeadline}</p>;
+    return (
+      <div className={this.props.small ? 'teaser small-teaser' : 'teaser'}>
+        <img src={this.props.src} alt="Image" className="teaser-image" />
+          <p className="top-headline">{this.props.topHeadline}</p>
+          <h2 className="big-headline">{this.props.bigHeadline}</h2>
+        {subHeadline}
+        {callToAction}
+      </div>
+    )
+  }
+}
+
+const mapStateToPageProps = function (state) {
+  return { overlayShown: state.showOverlay }
+};
+
+const mapDispatchToPageProps = function (dispatch) {
+  return {
+    hideOverlay: () => dispatch(hideOverlay()),
+    showOverlay: () => dispatch(showOverlay())
+  }
+};
 
 class Page extends Component {
+  toggleOverlay() {
+    if (this.props.overlayShown) {
+      this.props.hideOverlay()
+    } else {
+      this.props.showOverlay()
+    }
+  }
+
   render() {
+    const menuIcon = this.props.overlayShown ? close : burger;
     return (
       <div>
-        <Navbar>
-          <Navbar.Header>
-            <Navbar.Brand>
-              <IndexLink to="/">react-sandbox</IndexLink>
-            </Navbar.Brand>
-          </Navbar.Header>
-          <Nav>
-            <LinkContainer to="/categories">
-              <NavItem>Categories</NavItem>
-            </LinkContainer>
-            <LinkContainer to="/profile">
-              <NavItem>Profile</NavItem>
-            </LinkContainer>
-          </Nav>
-        </Navbar>
-        <Grid>
+        <section className="header">
+          <div className="container mobile-nav">
+            <div className="row">
+              <div className="twelve columns">
+                <img className="menu-icon" src={menuIcon} alt="Image" onClick={this.toggleOverlay.bind(this)} />
+                <span className="logo">DRINKLY</span>
+                <img className="shopping-cart" src={cart} />
+              </div>
+            </div>
+          </div>
+          <div className="container desktop-nav">
+            <div className="row">
+              <div className="four columns">
+                <span className="logo">DRINKLY</span>
+              </div>
+              <div className="four columns">
+                <input type="text" className="search-field" placeholder="What are you looking for?" />
+              </div>
+              <div className="four columns">
+                <img className="shopping-cart" src={cart} />
+                <span className="login"><a href="#">LOG IN / SIGN UP</a></span>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section className="content">
           {this.props.children}
-        </Grid>
+        </section>
+        <OverlayContainer />
       </div>
     );
   }
 }
 
+const PageContainer = connect(
+  mapStateToPageProps,
+  mapDispatchToPageProps
+)(Page);
+
 const mapStateToProps = (state) => {
   return {
-    counter: state
+    counter: state.counter
   }
 };
 
@@ -87,10 +159,58 @@ const HomeContainer = connect(
   mapDispatchToProps
 )(Home);
 
+const mapStateToOverlayProps = function (state) {
+  return {
+    show: state.showOverlay
+  }
+};
+
+class Overlay extends Component {
+  render () {
+    return this.props.show ? (
+      <div className="overlay">
+        <input type="text" className="search-field" placeholder="What are you looking for?" /><br/>
+        <span className="login"><a href="#">LOG IN / SIGN UP</a></span>
+      </div>
+    ) : null;
+  }
+}
+
+const OverlayContainer = connect(
+  mapStateToOverlayProps
+)(Overlay)
+
 class Categories extends Component {
   render() {
     return (
-      <p>Categories</p>
+      <section className="content">
+        <div className="container">
+          <div className="row">
+            <div className="twelve columns">
+              <span className="mobile-dropdown-link left">SHOP</span>
+              <span className="mobile-dropdown-link right">MAGAZINE</span>
+            </div>
+          </div>
+        </div>
+        <div className="container">
+          <div className="row">
+            <div className="seven columns">
+              <Teaser small={false} src="http://www.placehold.it/539x438" topHeadline="More than a digestive"
+                      bigHeadline="Enjoy our Selection of Finest Bitters"
+                      subHeadline="Stock your bar with exclusive Bitters to be ready for your next coctail party."
+                      callToAction="Shop now" />
+            </div>
+            <div className="five columns">
+              <Teaser small={true} src="http://www.placehold.it/379x228" topHeadline="Edinburgh at night"
+                      bigHeadline="Take a Walk on the Wild Side in Scotlands Beautiful Capital"
+                      callToAction="Read our travel guide" />
+              <Teaser small={true} src="http://www.placehold.it/379x228" topHeadline="Delicious"
+                      bigHeadline="You Deserve Some Very Special Desserts"
+                      callToAction="Read our recipes" />
+            </div>
+          </div>
+        </div>
+      </section>
     )
   }
 }
@@ -125,7 +245,7 @@ class App extends Component {
     return (
       <Provider store={store}>
         <Router history={browserHistory}>
-          <Route path="/" component={Page} auth={auth}>
+          <Route path="/" component={PageContainer} auth={auth}>
             <IndexRoute component={HomeContainer}/>
             <Route path="categories" component={Categories} />
             <Route path="profile" component={Profile} onEnter={this.requireAuth} />
